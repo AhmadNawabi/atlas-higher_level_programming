177de9ef5121dcb2a4e49005541dd417dc55b5fb
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-03 16:57:48 -0500

    update 1-last_digit.py file

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,6 @@
-import random as rd
-number = rd.randint(-1000, 1000)
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
 digit = abs(number) % 10
 if number < 0:
     digit = -digit
