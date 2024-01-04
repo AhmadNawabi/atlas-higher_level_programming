d8dcd17efdfd95b35ef87bdfb1581faa9a107767
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-04 12:10:46 -0500

    re-update 8-uppercase.py file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,3 @@ def uppercase(str):
         upper_char = chr(ord(upper) - 32) if 97 <= ord(upper) <= 122 else upper
         print("{}".format(upper_char), end="")
     print()
-
-
-uppercase("holberton")
