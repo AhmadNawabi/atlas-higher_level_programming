1bef11f90b2792ef0ddcaafb608d5582d1e0b6ae
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-04 12:02:00 -0500

    8-uppercase.py file

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for upper in str:
+        upper_char = chr(ord(upper) - 32) if 97 <= ord(upper) <= 122 else upper
+        print("{}".format(upper_char), end="")
+    print()
+
+
+uppercase()
