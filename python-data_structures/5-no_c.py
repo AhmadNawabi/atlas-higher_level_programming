315dd96462680ed91e1b4603170ef0a8bc6280ed
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-05 13:29:14 -0500

    update 5-no_c.py file

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     """Removing c and C from string """
 
     copyOfString = [copy for copy in my_string if "c" != copy != "C"]
-    print(''.join(copyOfString))
+    return (copyOfString)
