308f80524389d3cac78e8ea319bf62f2ced8f05b
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-05 21:40:37 -0500

    8-multiple_returns.py file

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-
-    # Check if the sentence is empty
-    if len(sentence) == 0:
-        return (0, None)# Return a tuple with length 0 and None as the first character
+    if len(sentence) == 0:  # Check if the sentence is empty
+        return (0, None)  # Return a tuple with length 0 and None as the first character
     else:
-        return (len(sentence), sentence[0])# Return a tuple with length of the sentence and its first character
+        return (len(sentence), sentence[0])  # Return a tuple with length of the sentence and its first character
