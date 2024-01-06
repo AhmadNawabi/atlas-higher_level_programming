153551ec40ba7be3964d98035d44d3ebd0bf5ebc
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-05 22:33:54 -0500

    update 10-divisible_by_2.py file

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    
     result_list = []
-
     for num in my_list:
         isDivisibleBy_2 = num % 2 == 0
         result_list.append(isDivisibleBy_2)
