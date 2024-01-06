9aba014d258d6917bc1ec5bec2a5681d34589446
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-05 22:32:40 -0500

    update 10-divisible_by_2.py file

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-     # Create a new list to store True/False values
+    
     result_list = []
 
-    # Iterate through the original list
     for num in my_list:
-        # Check if the number is a multiple of 2
         isDivisibleBy_2 = num % 2 == 0
         result_list.append(isDivisibleBy_2)
     return result_list
