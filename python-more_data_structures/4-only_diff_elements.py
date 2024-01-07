ee450f2e4d0d02013e095812f02d84c408360bec
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-07 10:24:31 -0500

    update 4-only_diff_elements.py file

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,7 +5,7 @@ def only_diff_elements(set_1, set_2):
 
     for element in set_1:
         if element != set_2:
-            common_elements.add(element)
+            common_elements.update(element)
             for other_element in set_2:
-                common_elements.add(other_element)
+                common_elements.update(other_element)
     return common_elements
