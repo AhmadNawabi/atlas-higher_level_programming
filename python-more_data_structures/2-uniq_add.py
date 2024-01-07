62449a57bd8575efd248601181eb1d801ae3f8ea
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-07 09:49:31 -0500

    2-uniq_add.py file

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+
+    unique_number = set()
+
+    for number in my_list:
+        unique_number.add(number)
+
+
+    sumOfNumber = sum(unique_number)
+
+    return sumOfNumber
