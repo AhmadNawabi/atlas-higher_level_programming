054224fd12c7a26f583e941d6bae70d7f38488c1
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-07 09:50:56 -0500

    updare 2-uniq_add.py file

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,11 +3,7 @@
 def uniq_add(my_list=[]):
 
     unique_number = set()
-
     for number in my_list:
         unique_number.add(number)
-
-
     sumOfNumber = sum(unique_number)
-
     return sumOfNumber
