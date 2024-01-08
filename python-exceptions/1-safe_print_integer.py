ad5c06c2b4d552cbde2fc5b98a8c3afac4f7216e
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-08 11:50:26 -0500

    re-update 1-safe_print_integer.py file

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print(int(value))
     except ValueError:
-         print("{} is not an integer".format(value))
          return False
     return True
