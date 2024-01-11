b7b32ff2dd36bedbe188595e18d1a5054da0e76e
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-11 11:50:14 -0500

    add whitespasec on 1-square.py file

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,28 +4,33 @@
 This module defines a Square class.
 '''
 
+
 class Square:
     """
     This class represents a square.
 
     Attributes:
-        __size (int): Private attribute representing the size of the square.
+        __size (int): Private attribute representing
+        the size of the square.
 
     Methods:
         __init__(self, size):
-            Initializes a new instance of the Square class with a specified size.
+            Initializes a new instance of the Square
+            class with a specified size.
 
     Example:
         # Create a square with a size of 5
         square_instance = Square(5)
 
     Note:
-        The size attribute (__size) is marked as private with a double underscore.
+        The size attribute (__size) is marked as 
+        private with a double underscore.
     """
 
     def __init__(self, size):
         """
-        Initializes a new instance of the Square class with a specified size.
+        Initializes a new instance of the Square
+        class with a specified size.
 
         Parameters:
             size (int): The size of the square.
