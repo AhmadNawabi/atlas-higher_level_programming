b6846e211f808dab98e76463af6df8db9e3e572f
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-11 11:13:32 -0500

    update adding more documents in 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 
+"""
+This module defines a Square class.
+
+The Square class represents a square geometric shape. It is a simple implementation
+with no specific attributes or methods defined.
+
+Example:
+    square_instance = Square()
+
+Attributes:
+    No attributes are defined in this empty class.
+
+Methods:
+    No methods are defined in this empty class.
+"""
 class Square:
     """
-    This class defines a square.
+    This class represents a square.
 
     Attributes:
         No attributes are defined in this empty class.
