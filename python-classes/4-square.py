1223f86a75f450c5efe0b86451079e679f56a35b
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-11 15:21:20 -0500

    4-square.py file

---
+++ python-classes/4-square.py
@@ -0,0 +1,101 @@
+#!/usr/bin/pyton3
+'''
+This module create a square class
+'''
+
+
+class Square:
+
+    """
+       This class represents a square.
+
+       Attributes:
+           __size (int): Private attribute
+            representing the size of the square.
+
+       Methods:
+           __init__(self, size):
+               Initializes a new instance of the
+                Square class with a specified size.
+
+       Example:
+           # Create a square with a size of 5
+           square_instance = Square(5)
+
+       Note:
+           The size attribute (__size) is marked
+            as private with a double underscore.
+       """
+
+    def __init__(self, size=0):
+        """
+            Initializes a new instance of the Square class with a specified size.
+
+            Parameters:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+        if not isinstance(self.__size, int):
+            '''
+            Attribute size must be an integer
+            Otherwise raises an exception "size must be an integer"
+            '''
+            raise TypeError("size must be an integer")
+
+        if self.__size < 0:
+            '''
+            Attribute size must be greater than 0
+            Otherwise raises an exception "size must be >= 0"
+            '''
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square using the property.
+
+        Parameters:
+             value (int): The new size to be set.
+
+        Raises:
+           TypeError: If the provided value is not an integer.
+           ValueError: If the provided value is less than 0.
+        """
+        if self.__size < 0:
+            self.__size = 0
+        elif self.__size > 1000:
+            self.__size = 1000
+        else:
+            self.__size = value
+
+        if not isinstance(value, int):
+            '''
+            Attribute size must be an integer
+            Otherwise raises an exception "size must be an integer"
+            '''
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            '''
+            Attribute size must be greater than 0
+            Otherwise raises an exception "size must be >= 0"
+            '''
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
