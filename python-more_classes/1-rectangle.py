a336fb4864609790d4f39715e7b5668fcb3565ed
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-14 21:24:01 -0500

    update 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,6 +23,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -37,3 +39,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
