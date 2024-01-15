3b84592f464eaa641e333b06c97800426ceaf196
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-15 11:14:10 -0500

    created 2-rectangle.py

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This class represents a rectangular"""
+
+
+class Rectangle:
+    """Initialize a rectangular object"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle
+            and make sure the width is greater than 0
+            and the value is integer.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle and make sure the
+        height is greater than 0 and the value is integer"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Calculate the area of the rectangle"""
+        return self.__width * self.height
+
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle"""
+        return 2 * self.__width + 2 * self.__height
