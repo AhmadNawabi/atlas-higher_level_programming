79e0d98b8dfb748580b58306474ceed417423154
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-19 18:23:41 -0500

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is an empty class
+"""
+
+
+class BaseGeometry:
+    """an empty class"""
+    pass
+
+    def area(self):
+        """This is an empty method
+        that raise an Exception
+        """
+        raise Exception('area() is not implemented')
