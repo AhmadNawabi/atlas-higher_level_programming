8d936400ed22afa9ea62ebcc0a12f38744ae3ce9
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-19 12:27:57 -0500

    update 0-lookup.py file

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python3
+"""
+Return a list of attributes and methods of the given object.
+"""
 
-class lookup(obj):
+
+def lookup(obj):
+    """
+    Return a list of attributes and methods of the given object.
+
+    Parameters:
+    - obj: Any object for which to retrieve attributes and methods.
+
+    Returns:
+    A list of strings representing the attributes and methods of the object.
+    The list includes only public attributes and methods, excluding internal
+    methods that start with '__'.
+
+    Example:
+    class ExampleClass:
+         def __init__(self, name):
+             self.name = name
+
+         def greet(self):
+            print(f"Hello, {self.name}!")
+
+    instance = ExampleClass("John")
+    lookup(instance)
+    ['greet', 'name']
+    """
     return dir(obj)
