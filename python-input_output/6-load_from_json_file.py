1c174ce706e1eb8ca9093b84449da8e6e6690eb2
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-23 14:51:58 -0500

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+import json
+"""
+from flask import json
+"""
+This function creates an Object from a “JSON file”
+"""
+
+
+def load_from_json_file(filename):
+    with open(filename) as file:
+        json_data = json.load(file)
+        return json_data
