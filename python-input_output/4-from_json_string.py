f09bd0f2df80b6e5e4696da5789d2b5111df7f35
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-23 13:04:12 -0500

    4-from_json_string.py file

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Import json
+"""
+import json
+"""
+    This function returns an object
+    (Python data structure) represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """
+    This function returns an object
+    (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
