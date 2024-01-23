ed018cc58b0c1caccfaea8c09f1d3ea275ef8982
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-22 21:17:20 -0500

    update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """Using with statement to make sure
     the file is closed automatic"""
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
