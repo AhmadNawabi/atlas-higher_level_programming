1d151179be53e391ce5b4f6f351425a7b1cc5417
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-24 00:04:07 -0500

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python3
-"""Load, add, save"""
-
-
+# 9-add_item.py
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-def add_items_to_list_and_save(arguments):
-    try:
-        """Load existing items from file"""
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        """If file doesn't exist, create an empty list"""
-        items = []
-
-    """Add new arguments to the list"""
-    items.extend(arguments)
-
-    """Save the updated list to the file"""
-    save_to_json_file(items, "add_item.json")
 
 if __name__ == "__main__":
-    """Get command-line arguments excluding the script name"""
-    arguments = sys.argv[1:]
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-    if arguments:
-        add_items_to_list_and_save(arguments)
-        print("Items added successfully and saved to add_item.json.")
-    else:
-        print("No items provided.")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
