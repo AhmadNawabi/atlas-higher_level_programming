13df2f55908379a5c136bc46f9572ec82ff09a9c
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-01-23 23:20:34 -0500

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-This module creates a script that adds args to Python
-list and saves them
-"""
+"""import sys"""
+import sys
+"""import save_to_json_file"""
+savefile = __import__('7-save_to_json_file').save_to_json_file
+"""import load_from_json_file"""
+loadfile = __import__('8-load_from_json_file').load_from_json_file
 
 
-from os import path
-from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""Load, Add, Save"""
 
-if path.exists("add_item.json") is False:
-    save_to_json_file([], "add_item.json")
-my_list = load_from_json_file("add_item.json")
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
-save_to_json_file(my_list, "add_item.json")
+
+if __name__ == '__main__':
+    try:
+        lst = loadfile("add_item.json")
+    except:
+        lst = []
+    for i in range(1, len(sys.argv)):
+        lst.append(sys.argv[i])
+    savefile(lst, "add_item.json")
+
