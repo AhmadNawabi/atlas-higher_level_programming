bbae938e989fc7af98f11db0edbd9ff61aabc662
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-02-20 18:29:12 -0500

    push the  1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,9 @@
--- creates the MySQL server user user_0d_1.
+-- Create the MySQL server user user_0d_1
 
--- Create user only if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
-
--- Flush privileges to apply changes
+CREATE USER
+	IF NOT EXISTS 'user_0d_1'@'localhost'
+		IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+	ON *.* TO 'user_0d_1'@'localhost'
+	IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
