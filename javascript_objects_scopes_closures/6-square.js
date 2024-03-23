7f609a156967202240bde0da358564bf3a26e756
AhmadNawabi <ahmadshamoon0505@gmail.com>
2024-03-23 11:05:02 -0400

    update the 6-square.js file and push it to the repo

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,12 +5,6 @@ module.exports = class Square extends require('./5-square.js') {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += c;
-      }
-      process.stdout.write(row + '\n');
-    }
+    for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
   }
 };
